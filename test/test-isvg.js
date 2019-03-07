a6ae52bab84235fa0da45c76362533150b156b84
CyDot <look@cydot.de>
2019-03-07 23:47:44 +0100

    Arranging

---
+++ test/test-isvg.js
@@ -0,0 +1,37 @@
+/*
+test-breakspaces.js
+Designer: © Michael Schwarz, CyDot
+Updated: 2019-01-09
+*/
+
+
+( function ( $ ) {
+    
+    function onDoc () { 
+        
+        $.isvg.inheritClass( false );
+        $.isvg.inheritId( true );
+        $.isvg.fallbackFormat( "png" );
+        
+        $.isvg( onISVG );
+        
+        $.isvg.replace( ".target", "test/cydot-logo.svg", onInsert );
+        
+        //$.isvg.before( ".target", "test/cydot-logo.svg", onInsert );
+        
+        //$.isvg.after( ".target", "test/cydot-logo.svg", onInsert );
+    }
+    
+    function onISVG ( e ) {
+        console.log( "onISVG", e );
+    }
+    
+    function onInsert ( e ) {
+        console.log( "onInsert", e );
+    }
+    
+    // -----------------------------------------------
+    
+    $( document ).ready( onDoc );
+    
+})( jQuery );
